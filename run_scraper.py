68e0a58f9196268dd1cf0947760f841458b1bae1
Niyati Bafna <niyatibafna13@gmail.com>
2022-01-25 14:45:45 +0100

    Paginating for each query term until a max of 10000 tweets

--- run_scraper.py
+++ run_scraper.py
@@ -1,21 +1,34 @@
 #Calls scraper class over different queries
-
+import os
+import time
 from scraper import *
 
 CRED_FILEPATH = "credentials/credentials.config"
-WORDLIST_FPATH = ""
+WORDLIST_FPATH = "wordlists/islamic_religious_terms.txt"
 COUNTRY_CODE = "IN"
-MAX_RESULTS = 10000
+MAX_RESULTS = 500
+OUT_DATA_FPATH = "data/crawled/"
 
 def get_wordlist(filepath):
     with open(filepath, "r") as wl:
         return [word.strip() for word in wl.read().split("\n") if len(word)!=0]
 
-wordlist = get_wordlist(WORDLIST_FPATH)
-years = range(2010, 2022, 2)
 
-for q_term in wordlist:
-    for year in years:
-        scraper = Scraper(CRED_FILEPATH)
+wordlist = get_wordlist(WORDLIST_FPATH)
+print("WORDLIST: ", wordlist[:5])
+wordlist = wordlist[:1]
+# print(wordlist)
+# wordlist = ["makaan"]
+years = list(range(2010, 2023)) # + list(range(2018, 2022))
+# years = [2018]
+scraper = Scraper(CRED_FILEPATH)
+
+for year in years:
+    year_dpath = os.path.join(OUT_DATA_FPATH, str(year))
+    if not os.path.isdir(year_dpath):
+        os.mkdir(year_dpath)
+    for q_term in wordlist:
+        print("SEARCHING FOR {} in the year {}".format(q_term, year))
         scraper.crawl(q_term, year, year+1, COUNTRY_CODE, MAX_RESULTS)
-        scraper.save()
+        scraper.save(os.path.join(year_dpath, q_term+".json"))
+        # time.sleep(1)
--- scraper.py
+++ scraper.py
@@ -2,25 +2,33 @@
 import requests
 from requests.structures import CaseInsensitiveDict
 import configparser
+import json
+import time
+SLEEP_TIME = 2
 
 class Scraper:
-
+    global URL
     URL = "https://api.twitter.com/2/tweets/search/all"
 
     def __init__(self, cred_filepath):
 
         self.bearer_token = self.get_credentials(cred_filepath)
-        # self.query_exp = query_phrase
-        # self.start_year = start_year
-        # self.end_year = end_year
-        # self.country_code = country_code
-        # self.max_results = max_results
         self.results = dict()
 
     def get_credentials(self, cred_filepath):
-        config = configparser.Configparser()
+        config = configparser.RawConfigParser()
         config.read(cred_filepath)
         return config['twitterAuth']['bearer_token']
+
+    def append_object(self, new_results):
+        if "meta" in new_results and new_results["meta"]["result_count"] != 0:
+            self.results["data"].extend(new_results["data"])
+            self.results["includes"]["users"].extend(new_results["includes"]["users"])
+            self.results["includes"]["places"].extend(new_results["includes"]["places"])
+            self.results["meta"]["result_count"] += new_results["meta"]["result_count"]
+
+            assert self.results["meta"]["result_count"] == len(self.results["data"])
+
 
     def crawl(self, query_exp, start_year, end_year, country_code, max_results):
         #Crawl and save in self.results
@@ -40,7 +48,21 @@ class Scraper:
                 "user.fields":"created_at,location", \
                 "max_results":max_results}
 
-        self.results = requests.get(URL, params=rule, headers=headers)
+        self.results = requests.get(URL, params=rule, headers=headers).json()
+        time.sleep(SLEEP_TIME)
+
+        new_results = self.results
+        while "meta" in new_results and "next_token" in new_results["meta"]:
+            print("(Paginating) Total tweets: {}".format(self.results["meta"]["result_count"]))
+            # print(self.results)
+            rule["next_token"] = new_results["meta"]["next_token"]
+            # print(rule)
+            new_results = requests.get(URL, params=rule, headers=headers).json()
+            time.sleep(SLEEP_TIME)
+            self.append_object(new_results)
+            if self.results["meta"]["result_count"] > 10000:
+                break
+
 
     def save(self, filepath):
         #Save returned data to specified filepath (including "data" and "includes" fields)
