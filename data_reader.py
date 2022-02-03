b0f7db7ada8a9bb3b2b16fdcf131bc720a084b64
Niyati Bafna <niyatibafna13@gmail.com>
2022-02-03 19:52:24 +0100

    Reading negative examples only from other directories of HASOC data

--- data_reader.py
+++ data_reader.py
@@ -9,7 +9,16 @@ class TweetReader:
     def __init__(self, path_to_crawled_data):
         self.CRAWLED_DATA_DIR = path_to_crawled_data
 
-    def filter_good(self, text):
+    def filter_good(self, tweet, geoid_place_map):
+        if " RT " in tweet["text"]:
+            return False
+
+        if "geo" in tweet and "place_id" in tweet["geo"]:
+            if geoid_place_map[tweet["geo"]["place_id"]] != "IN":
+                return False
+        else:
+            return False
+
         return True
 
     def reader(self, years = None, keywords = None):
@@ -33,15 +42,29 @@ class TweetReader:
                 with open("{}/{}/{}".format(self.CRAWLED_DATA_DIR,year,fpath), "r", encoding = "utf-8") as f:
                     crawled_data = json.load(f)
 
+                if "data" not in crawled_data:
+                    continue
+                try:
+                    geoid_place_map = {place_dict["id"]:place_dict["country_code"] for place_dict in crawled_data["includes"]["places"]}
+                    # print(len(geoid_place_map))
+                except:
+                    geoid_place_map = dict()
+                    print("Place map not loaded")
+
+                if "IN" not in geoid_place_map.values():
+                    continue
+
                 for tweet in crawled_data["data"]:
-                    if self.filter_good(tweet["text"]):
+                    if self.filter_good(tweet, geoid_place_map):
                         id_tweet_map[id] = tweet["text"]
                         id += 1
 
+                print("Read {} from year {}".format(fpath,  year))
+                print("Collected data: ", len(id_tweet_map))
 
         return id_tweet_map
 
 
-# tr = TweetReader()
-# id_tweet_map = tr.reader([2010, 2011],["halal"])
+# tr = TweetReader("data/crawled")
+# id_tweet_map = tr.reader(range(2010, 2020))
 # print(len(id_tweet_map))
--- hasoc_reader.py
+++ hasoc_reader.py
@@ -47,18 +47,24 @@ class HasocReader:
 
         text_id = max(list(id_tweet_map.keys())) + 1 if id_tweet_map else 0
 
+        only_take_negatives = False
         for topic_dir in [f for f in os.listdir(self.HASOC_DIR_CM) if os.path.isdir("{}/{}".format(self.HASOC_DIR_CM, f))]:
+            if topic_dir not in ["casteism", "religious controversies", "indian politics"]:
+                only_take_negatives = True
+            print("Reading: ", topic_dir)
             for subdir in [f for f in os.listdir("{}/{}".format(self.HASOC_DIR_CM, topic_dir)) if os.path.isdir("{}/{}/{}".format(self.HASOC_DIR_CM, topic_dir, f))]:
                 hasoc_id_tweet = self.read_tweets("{}/{}/{}/{}".format(self.HASOC_DIR_CM, topic_dir, subdir, "data.json"))
                 hasoc_id_label = self.read_labels("{}/{}/{}/{}".format(self.HASOC_DIR_CM, topic_dir, subdir, "labels.json"))
                 for hasoc_id, tweet in hasoc_id_tweet.items():
-                    try:
-                        id_tweet_map[text_id] = tweet
-                        id_class_map[text_id] = hasoc_id_label[hasoc_id]
-                        text_id += 1
-                    except:
-                        print("Label not found!")
-                        pass
+                    if (only_take_negatives and hasoc_id_label[hasoc_id] == 0) or (not only_take_negatives):
+                        try:
+                            id_tweet_map[text_id] = tweet
+                            id_class_map[text_id] = hasoc_id_label[hasoc_id]
+                            text_id += 1
+                        except:
+                            print("Label not found!")
+                            pass
+            only_take_negatives = False
 
         return id_tweet_map, id_class_map
 
@@ -67,20 +73,26 @@ class HasocReader:
         DIRS = [self.HASOC_DIR_EN, self.HASOC_DIR_HI]
         for DIR in DIRS:
             for fpath in [f for f in os.listdir(DIR) if f.startswith("hasoc")]:
+                if len(id_tweet_map) > 12000:
+                    break
                 # print("{}/{}".format(DIR, fpath))
                 data = pd.read_excel("{}/{}".format(DIR, fpath))
                 text_id = max(list(id_tweet_map.keys())) + 1 if id_tweet_map else 0
                 for idx in range(len(data['text'])):
+                    if self.label_mapper(data['task1'][idx]) == 1:
+                        continue
                     id_tweet_map[text_id] = data['text'][idx]
                     id_class_map[text_id] = self.label_mapper(data['task1'][idx])
                     text_id += 1
+
 
         return id_tweet_map, id_class_map
 
 
     def reader(self, id_tweet_map = dict(), id_class_map = dict()):
         id_tweet_map, id_class_map = self.cm_reader(id_tweet_map, id_class_map)
-        # id_tweet_map, id_class_map = self.hi_en_reader(id_tweet_map, id_class_map)
+        print("Length of positive examples: {}".format(len([val for val in id_class_map.values() if val == 1])))
+        id_tweet_map, id_class_map = self.hi_en_reader(id_tweet_map, id_class_map)
 
         return id_tweet_map, id_class_map
 
